a6698c87e062e3df6a1f22a648323453f6a7b73b
Mrs.Dukes <mrs.knit.wit.dukes@gmail.com>
2021-08-01 17:25:37 -0700

    addition of js code plus updates to index and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,5 +34,156 @@ var formSubmitHandler = function (event) {
     else {
         alert("Please enter a City name");
     }
-    
 }
+
+// get weather information from OpenWeather
+
+var getWeatherInfo = function (cityname) {
+    var apiCityUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=f97301447cbd41068af8623a398ba1fb";
+    fetch(
+        // make a fetch request using city name to get latitude and longitude for city
+        apiCityUrl
+    )
+        .then(function (cityResponse) {
+            return cityResponse.json();
+        })
+        .then(function (cityResponse) {
+            // create variables to hold the latitude and longitude or requested city
+            console.log(cityResponse)
+            var latitude = cityResponse.coord.lat;
+            var longitude = cityResponse.coord.lon;
+
+            // create variables for City name, current date and icon information for use in current Weather heading
+
+            var city = cityResponse.name;
+            var date = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
+            var weatherIcon = cityResponse.weather[0].icon;
+            var weatherDescription = cityResponse.weather[0].description;
+            var weatherIconLink = "<img src='http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png' alt='" + weatherDescription + "' title='" + weatherDescription + "'  />"
+
+            // empty Current Weather element for new data
+            currentWeatherCardEl.textContent = "";
+            fiveDayEl.textContent = "";
+
+            // update <h2> element to show city, date and icon
+
+            weatherStatusEl.innerHTML = city + " (" + date + ") " + weatherIconLink;
+
+            // reamove class name 'hidden' to show current weather card
+            currentWeatherEl.classList.remove("hidden");
+            fiveDayCardEl.classList.remove("hidden");
+
+            // return a fetch request to the OpenWeather using longitude and latitude from previous fecth
+
+            return fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=alerts,minutely,hourly&units=imperial&appid=f97301447cbd41068af8623a398ba1fb');
+        })
+        .then(function (response) {
+            // return response in json format
+            return response.json();
+        })
+        .then(function (response) {
+            console.log(response);
+            // send response data to displayWeather function for final display
+            displayWeather(response);
+        });
+};
+
+// display the weather on page
+var displayWeather = function (weather) {
+    // check if api returned any weather data
+    if (weather.length === 0) {
+        weatherContainerEl.textContent = "No weather data found.";
+        return;
+    }
+    // create temperature element
+    var temperature = document.createElement('p');
+    temperature.id = "temperature";
+    temperature.innerHTML = "<strong>Temperature:</strong> " + weather.current.temp.toFixed(1) + "°F";
+    currentWeatherEl.appendChild(temperature);
+
+    // create Humidity element
+    var humidity = document.createElement('p');
+    humidity.id = "humidity";
+    humidity.innerHTML = "<strong>Humidity:</strong> " + weather.current.humidity + "%";
+    currentWeatherEl.appendChild(humidity);
+
+    // create Wind Speed element
+    var windSpeed = document.createElement('p');
+    windSpeed.id = "wind-speed";
+    windSpeed.innerHTML = "<strong>Wind Speed:</strong> " + weather.current.wind_speed.toFixed(1) + " MPH";
+    currentWeatherEl.appendChild(windSpeed);
+
+    // create uv-index element
+    var uvIndex = document.createElement('p');
+    var uvIndexValue = weather.current.uvi.toFixed(1);
+    uvIndex.id = "uv-index";
+    if (uvIndexValue >= 0) {
+        uvIndex.className = "uv-index-green"
+    }
+    if (uvIndexValue >= 3) {
+        uvIndex.className = "uv-index-yellow"
+    }
+    if (uvIndexValue >= 8) {
+        uvIndex.className = "uv-index-red"
+    }
+    uvIndex.innerHTML = "<strong>UV Index:</strong> <span>" + uvIndexValue + "</span>";
+    currentWeatherEl.appendChild(uvIndex);
+
+    // get extended forecast date
+    var forecastArray = weather.daily;
+
+    // create day cards for extended forecast
+    for (let i = 0; i < forecastArray.length - 3; i++) {
+        var date = (today.getMonth() + 1) + '/' + (today.getDate() + i + 1) + '/' + today.getFullYear();
+        var weatherIcon = forecastArray[i].weather[0].icon;
+        var weatherDescription = forecastArray[i].weather[0].description;
+        var weatherIconLink = "<img src='http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png' alt='" + weatherDescription + "' title='" + weatherDescription + "'  />"
+        var dayEl = document.createElement("div");
+        dayEl.className = "day";
+        dayEl.innerHTML = "<p><strong>" + date + "</strong></p>" +
+            "<p>" + weatherIconLink + "</p>" +
+            "<p><strong>Temp:</strong> " + forecastArray[i].temp.day.toFixed(1) + "°F</p>" +
+            "<p><strong>Humidity:</strong> " + forecastArray[i].humidity + "%</p>"
+
+        fiveDayEl.appendChild(dayEl);
+
+    }
+
+}
+
+// load any past city weather searches
+var loadHistory = function () {
+    searchArray = JSON.parse(localStorage.getItem("weatherSearch"));
+
+    if (searchArray) {
+        searchHistoryArray = JSON.parse(localStorage.getItem("weatherSearch"));
+        for (let i = 0; i < searchArray.length; i++) {
+            var searchHistoryEl = document.createElement('button');
+            searchHistoryEl.className = "btn";
+            searchHistoryEl.setAttribute("data-city", searchArray[i])
+            searchHistoryEl.innerHTML = searchArray[i];
+            historyButtonsEl.appendChild(searchHistoryEl);
+            historyCardEl.removeAttribute("style");
+        }
+    }
+}
+
+// search weather using search history buttons
+var buttonClickHandler = function (event) {
+    var cityname = event.target.getAttribute("data-city");
+    if (cityname) {
+        getWeatherInfo(cityname);
+    }
+}
+
+// clear search history
+var clearHistory = function (event) {
+    localStorage.removeItem("weatherSearch");
+    historyCardEl.setAttribute("style", "display: none");
+}
+
+cityFormEl.addEventListener("submit", formSubmitHandler);
+historyButtonsEl.addEventListener("click", buttonClickHandler);
+trashEl.addEventListener("click", clearHistory);
+
+loadHistory();
